079974d16f2b89ebe56a4919285525d22ad2f626
tainio <clomwell@gmail.com>
2015-06-14 17:22:05 +0900

    geocode

--- app/src/main/java/com/example/tainio/myweather/MainActivity.java
+++ app/src/main/java/com/example/tainio/myweather/MainActivity.java
@@ -1,5 +1,11 @@
 package com.example.tainio.myweather;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +22,17 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private String data;
+    private LocationManager locationmanager = null;
     private String selectcity;
+    TextView lmtv=null;
+    TextView rtv=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +41,9 @@ public class MainActivity extends ActionBarActivity {
 
         Spinner spin = (Spinner) findViewById(R.id.spinner);
         Button button = (Button) findViewById(R.id.resultbutton);
-        final TextView tv = (TextView) findViewById(R.id.resultView);
+        lmtv = (TextView) findViewById(R.id.lmtv);
+        rtv = (TextView) findViewById(R.id.resultView);
+        locationmanager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,11 +53,11 @@ public class MainActivity extends ActionBarActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        data= getXmlData();
+                        data = getXmlData();
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tv.setText(data);
+                                rtv.setText(data);
                             }
                         });
                     }
@@ -62,7 +75,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectcity = parent.getItemAtPosition(position).toString();
-                tv.setText(selectcity);
+                rtv.setText(selectcity);
             }
 
             @Override
@@ -148,5 +161,75 @@ public class MainActivity extends ActionBarActivity {
         buffer.append("end Weather XML parsing...\n");
 
         return buffer.toString(); //StringBuffer 문자열 객체 반환
+    } //getxmldata() end
+
+    public void onPause(){
+        super.onPause();
+        locationmanager.removeUpdates(netListener);
     }
+
+    public void onResume(){
+        super.onResume();
+        locationmanager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 10, netListener);
+    }
+
+    LocationListener netListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lat = location.getLatitude();
+            double lng = location.getLongitude();
+
+            String address = getAddress(lat,lng);
+            lmtv.setText(address);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    /** 위도와 경도 기반으로 주소를 리턴하는 메서드*/
+    public String getAddress(double lat, double lng){
+        String address = null;
+
+        //위치정보를 활용하기 위한 구글 API 객체
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+
+        //주소 목록을 담기 위한 HashMap
+        List<Address> list = null;
+
+        try{
+            list = geocoder.getFromLocation(lat, lng, 1);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(list == null){
+            Toast.makeText(getApplicationContext(), "주소 못찾음",Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        if(list.size() > 0){
+            Address addr = list.get(0);
+            address = addr.getCountryName() + " "
+                    + addr.getPostalCode() + " "
+                    + addr.getLocality() + " "
+                    + addr.getThoroughfare() + " "
+                    + addr.getFeatureName();
+        }
+
+        return address;
+    }
+
 }
